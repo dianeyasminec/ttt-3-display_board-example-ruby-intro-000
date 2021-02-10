4f4015fb555d4ec776a39fc66d9271fbd4baf46d
Diane Compaore <dianeyasminec60@gmail.com>
2021-02-10 23:11:06 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,13 +1,13 @@
 def display_board
 puts  "Welcome to Tic Tac Toe Board"
-2.times  {print " "+"|"+" "}
+2.times  {print "   "+"|"+"   "}
 puts
- 7.times {print "-"}
+ 11.times {print "-"}
  puts
- 2.times  {print " "+"|"+" "}
+ 2.times  {print "   "+"|"+"   "}
  puts
-  7.times {print "-"}
+  11.times {print "-"}
   puts
-  2.times  {print " "+"|"+" "}
+  2.times  {print "   "+"|"+"   "}
 end
  display_board
